68d8895a10db6d1d7cf3a01eee16ae334b5583fe
softkisik228 <122627181+softkisik228@users.noreply.github.com>
2024-02-27 18:48:09 +0300

    feat: added func to move right

--- main.py
+++ main.py
@@ -38,12 +38,11 @@ class GameWindow:
                     if event.type == pygame.QUIT:
                         pygame.quit()
                     elif event.type == pygame.KEYDOWN:
-                        print(1)
                         self.board = [[0] * self.width for _ in range(self.height)].copy()
                         running = False
                 pygame.display.flip()
-            fig = Figure()
-            fig.create_figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
+            global fig
+            fig = Figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
             return True
 
                 
@@ -76,8 +75,19 @@ class GameWindow:
                 for x in range(self.width):
                     if self.board[y][x] and not self.board[y][x].get_die():
                         self.board[y][x], self.board[y + 1][x] = self.board[y + 1][x], self.board[y][x]
+        
+    def get_board(self):
+        return self.board
                     
-                        
+    def move_figure_right(self, id):
+        print(id)
+        copy_board = self.board.copy()
+        for y in range(self.height):
+            for x in range(self.width - 1, -1, -1):
+                if copy_board[y][x]:
+                    print(copy_board[y][x].get_id())
+                if (copy_board[y][x] and copy_board[y][x].get_id() == id):
+                    self.board[y][x], self.board[y][x + 1] = 0, self.board[y][x]
 
 
 
@@ -85,7 +95,7 @@ class Ceil:
     def __init__(self, color, last_id):
         self.color = color
         self.die = False
-        self.id_figure = last_id + 1
+        self.id_figure = last_id
     
     def get_color(self) -> tuple:
         return self.color
@@ -108,11 +118,11 @@ class Ceil:
                 if board[y][x]:
                     if board[y][x].get_id() == self.id_figure:
                         board[y][x].dieing()
-        fig = Figure()
-        fig.create_figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
+        global fig
+        fig = Figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
 
 class Figure:
-    def __init__(self):
+    def __init__(self, type, color, id):
         self.types = {0: [[0, 3], [0, 4], [0, 5], [0, 6]], 
                       1: [[0, 3], [0, 4], [0, 5], [1, 3]],
                       2: [[0, 3], [0, 4], [0, 5], [1, 5]],
@@ -120,18 +130,26 @@ class Figure:
                       4: [[1, 4], [0, 4], [0, 5], [1, 5]],
                       5: [[1, 6], [0, 4], [0, 5], [1, 5]],
                       6: [[1, 4], [0, 4], [0, 5], [1, 3]],}
-    
-    def create_figure(self, type, color, id):
         for i in self.types[type]:
             cel = Ceil(color, id)
             if board.set_ceil(i[1], i[0], cel):
                 break
             self.type = type
+            self.id = id
+            global last_id
+            last_id += 1
 
-
-    def move_right(self):
-        pass
-            
+    def move_right(self, width, height, copy_board):
+        can_i_move = True
+        for y in range(height):
+            for x in range(width):
+                if x != width - 1:
+                    if (copy_board[y][x] and copy_board[y][x].get_id() == self.id) and (copy_board[y][x + 1] and (copy_board[y][x + 1].get_id() != self.id)):
+                        can_i_move = False
+                elif copy_board[y][x] and copy_board[y][x].get_id() == self.id:
+                    can_i_move = False
+        if can_i_move:
+            board.move_figure_right(self.id)
 
 
 if __name__ == '__main__':
@@ -147,18 +165,20 @@ if __name__ == '__main__':
 
     last_id = 0
 
-    board = GameWindow(10, 20)
+    width = 10
+    height = 20
+
+    board = GameWindow(width, height)
     
-    fig = Figure()
-    fig.create_figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
+    fig = Figure(randint(0, 6), (randint(0, 255), randint(0, 255), randint(0, 255)), last_id)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    pass
+                if event.key == pygame.K_RIGHT:
+                    fig.move_right(width, height, board.get_board())
         board.render(screen, size)
         board.cells_down()
         clock.tick(fps)
